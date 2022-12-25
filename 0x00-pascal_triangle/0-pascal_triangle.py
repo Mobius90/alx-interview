cca96bd97ce6b559c59df029f77d02384a62e85c
Mobius90 <mikiyasdaniel1@gmail.com>
2022-12-25 15:31:41 -0600

    question 0

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,14 +3,15 @@
 
 
 def pascal_triangle(n):
-        if(n == 0):
-            return []
-        triangle = [[1]]
-        for n in range(1, n):
-            thisRow = []
-            thisRow.append(1)
-            for k in range(1, n):                
-                thisRow.append(triangle[n-1][k-1] + triangle[n-1][k])
-            thisRow.append(1)
-            triangle.append(thisRow)
-        return triangle
+    '''n,k = n-1,k-1 + n-1,k'''
+    if(n == 0):
+        return []
+    triangle = [[1]]
+    for n in range(1, n):
+        thisRow = []
+        thisRow.append(1)
+        for k in range(1, n):                
+            thisRow.append(triangle[n-1][k-1] + triangle[n-1][k])
+        thisRow.append(1)
+        triangle.append(thisRow)
+    return triangle
